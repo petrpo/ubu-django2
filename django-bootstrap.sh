bdebdc0dae133b93a59f2224fa3ce0cbe3492850
PetrPo <petrpo.reg@gmail.com>
2019-06-09 11:15:17 +0200

    some echos added
    Echos for informing user

--- django-bootstrap.sh
+++ django-bootstrap.sh
@@ -2,12 +2,19 @@
 # basic django bootstrap (ver.2)
 #
 
+echo "Should be system updated? YES !!" 
+sleep 3
+
 sudo aptitude update
 sudo aptitude -y upgrade
 #
 # #<python-pip> is not neccesary for django (ver.2)
 #sudo aptitude install python-pip
 #
+echo
+echo "System will bootstrap files for django2 under python3" 
+sleep 3
+
 sudo aptitude install python3-pip
 
 # time for pip3
