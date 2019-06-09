bbefd6965f60d0fc80c8310e8249dc7598f88f01
PetrPo <petrpo.reg@gmail.com>
2019-06-09 13:09:12 +0200

    corrected typo in if condition
    corrected typo in if condition

--- django-bootstrap.sh
+++ django-bootstrap.sh
@@ -2,7 +2,7 @@
 # basic django bootstrap (ver.2) under ubuntu linux
 #
 
-echo "Should the system be updated? YES !!" 
+echo "Should the system be updated? YES !!"
 sleep 3
 
 sudo aptitude update
@@ -12,7 +12,7 @@ sudo aptitude -y upgrade
 #sudo aptitude install python-pip
 #
 echo
-echo "System will bootstrap files for django2 under python3" 
+echo "System will bootstrap files for django2 under python3"
 sleep 3
 
 #
@@ -24,7 +24,7 @@ sudo aptitude -y install python3-pip
 # pip3 installation
 #
 echo
-echo "pip3 upgrade, download virtualenv, django" 
+echo "pip3 upgrade, download virtualenv, django"
 sleep 3
 sudo -H pip3 install --upgrade pip
 sudo -H pip3 install virtualenv
@@ -35,7 +35,7 @@ sudo -H pip3 install django
 #
 echo "Should I create your dev directory? (y,yes,Y,Yes,YES|No or hit enter)"
 read YES_or_NO
-if [ $YES_or_NO == "y" ] || [ $YES_or_NO == "Y"] || [ $YES_or_NO == "yes"] ||  [ $YES_or_NO == "Yes"] ||  [ $YES_or_NO == "YES"] ; then
+if [ $YES_or_NO == "y" ] || [ $YES_or_NO == "Y" ] || [ $YES_or_NO == "yes" ] || [ $YES_or_NO == "Yes" ] || [ $YES_or_NO == "YES" ] ; then
   echo "y,Y,yes,Yes,YES"
   echo
   echo "How Should I name devel directory"
@@ -51,14 +51,18 @@ if [ $YES_or_NO == "y" ] || [ $YES_or_NO == "Y"] || [ $YES_or_NO == "yes"] ||  [
     mkdir ~/$MY_DEV_DIR
   fi
   cd ~/$MY_DEV_DIR
-  # create your project in virtual environment
-  virtualenv my-py-project01
+  # create your project in virtual environment in devel directory
+  echo "What is your python project name ??"
+  read $PY_PROJECT
+  virtualenv $PY_PROJECT
+  echo
+  echo "Your project $PY_PROJECT is created"
   #
   # import django module and check django's version
   #
   python3 -c "import django"
   echo
-  echo "django version:"
+  echo "django version in python3:"
   python3 -m django --version
   echo
 fi
